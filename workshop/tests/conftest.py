4eefd00add5ccd357de49ec9efd2205097611551
GhostFantik <ghostfantik@gmail.com>
2021-03-30 00:36:52 +0500

    fix test deployment

--- workshop/tests/conftest.py
+++ workshop/tests/conftest.py
@@ -7,10 +7,9 @@ from workshop.settings import settings
 @pytest.fixture(scope='session')
 def temp_db():
     if settings.PRODUCTION:
-        db_url = settings.production_test_db_connect
-    else:
-        db_url = settings.test_db_connect
+        return
 
+    db_url = settings.test_db_connect
     create_database(db_url)
 
     try:
