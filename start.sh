84d47606766ee52a871af43a34d0890b2ff97f2b
GhostFantik <ghostfantik@gmail.com>
2021-03-30 00:23:36 +0500

    wait postgres

--- start.sh
+++ start.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
+
+
+until PGPASSWORD=123456 psql -h "db-test" -U "postgres" -c '\q'; do
+  >&2 echo "Postgres TEST-DB is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres TEST-DB is up - executing command"
 pytest > ./test_log.txt
+
+until PGPASSWORD=123456 psql -h "db" -U "postgres" -c '\q'; do
+  >&2 echo "Postgres PRODUCTION is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres PRODUCTION is up - executing command"
 python -m workshop
